c28eeac18cdc6e9a8bc38f816c4a33931b40dcc2
fjc0k <fjc0kb@gmail.com>
2018-04-14 22:05:27 +0800

    feat(test): add case

--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,119 @@
-test('test', () => {
-  expect(1).toBe(1)
+import mergeData from '../src'
+
+const target = {
+  directives: [{
+    name: 'show',
+    value: false
+  }],
+  ref: 'button',
+  staticClass: 'component',
+  class: ['button', {
+    mini: true
+  }],
+  staticStyle: {
+    width: '20px',
+    height: '10px'
+  },
+  style: ({
+    color: 'red',
+    fontSize: '1em'
+  }),
+  attrs: {
+    id: 'btn',
+    type: 'submit'
+  },
+  domProps: {
+    innerHTML: 'html',
+    textContent: 'text'
+  },
+  on: {
+    click: 1,
+    touchstart: 1
+  },
+  nativeOn: {
+    click: 1,
+    touchstart: 1
+  }
+}
+
+const source = {
+  styleName: '@button',
+  style: 'font-size:2em',
+  staticStyle: {
+    width: '10px'
+  },
+  staticClass: 'btn2 btn-disabled',
+  class: 'btn3',
+  attrs: {
+    type: 'button',
+    disabled: true
+  },
+  domProps: {
+    innerHTML: 'new html'
+  },
+  on: {
+    click: 2,
+    touchend: 1
+  },
+  nativeOn: {
+    touchstart: 2
+  }
+}
+
+const result = {
+  attrs: {
+    disabled: true,
+    id: 'btn',
+    type: 'button'
+  },
+  class: [
+    'btn3',
+    'button',
+    {
+      mini: true
+    }
+  ],
+  directives: [
+    {
+      name: 'show',
+      value: false
+    }
+  ],
+  domProps: {
+    innerHTML: 'new html',
+    textContent: 'text'
+  },
+  nativeOn: {
+    click: 1,
+    touchstart: [
+      2,
+      1
+    ]
+  },
+  on: {
+    click: [
+      2,
+      1
+    ],
+    touchend: 1,
+    touchstart: 1
+  },
+  ref: 'button',
+  staticClass: 'component btn2 btn-disabled',
+  staticStyle: {
+    height: '10px',
+    width: '10px'
+  },
+  style: [
+    'font-size:2em',
+    {
+      color: 'red',
+      fontSize: '1em'
+    }
+  ],
+  styleName: '@button'
+}
+
+test('The merged data is correct.', () => {
+  expect(mergeData(target, source)).toEqual(result)
 })
