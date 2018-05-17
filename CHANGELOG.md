e8a1a93602821232b4ce3a67e75351c311d09601
fjc0k <fjc0kb@gmail.com>
2018-05-17 15:20:40 +0800

    chore(release): 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,16 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+<a name="0.2.0"></a>
+# [0.2.0](https://github.com/fjc0k/vue-merge-data/compare/v0.1.3...v0.2.0) (2018-05-17)
+
+
+### Features
+
+* support multiple sources ([0ba5819](https://github.com/fjc0k/vue-merge-data/commit/0ba5819))
+
+
+
 <a name="0.1.3"></a>
 ## [0.1.3](https://github.com/fjc0k/vue-merge-data/compare/v0.1.2...v0.1.3) (2018-04-18)
 
--- dist/vue-merge-data.js
+++ dist/vue-merge-data.js
@@ -1,5 +1,5 @@
 /*!
- * vue-merge-data v0.1.3
+ * vue-merge-data v0.2.0
  * (c) 2018-present fjc0k <fjc0kb@gmail.com> (https://github.com/fjc0k)
  * Released under the MIT License.
  */
@@ -29,60 +29,69 @@
     return target;
   };
 
-  function mergeData(target, source) {
-    // Shallow copy target
-    target = assign({}, target); // Merge
+  function mergeData() {
+    var args = [].slice.call(arguments);
+    var target = assign({}, args[0]);
+    var sources = args.slice(1);
 
-    assign(target, source, function (propName) {
-      var targetValue = target[propName];
-      var sourceValue = source[propName];
+    var _loop = function _loop(i) {
+      var source = sources[i];
+      assign(target, source, function (propName) {
+        var targetValue = target[propName];
+        var sourceValue = source[propName];
 
-      if (targetValue) {
-        switch (propName) {
-          // append
-          case 'staticClass':
-            target[propName] = (targetValue + ' ' + sourceValue).trim();
-            break;
-          // override
+        if (targetValue) {
+          switch (propName) {
+            // append
+            case 'staticClass':
+              target[propName] = (targetValue + ' ' + sourceValue).trim();
+              break;
+            // override
 
-          case 'attrs':
-          case 'domProps':
-          case 'scopedSlots':
-          case 'staticStyle':
-          case 'props':
-          case 'hook':
-          case 'transition':
-            assign(targetValue, sourceValue);
-            break;
-          // expand
+            case 'attrs':
+            case 'domProps':
+            case 'scopedSlots':
+            case 'staticStyle':
+            case 'props':
+            case 'hook':
+            case 'transition':
+              assign(targetValue, sourceValue);
+              break;
+            // expand
 
-          case 'class':
-          case 'style':
-          case 'directives':
-            target[propName] = [].concat(sourceValue, targetValue);
-            break;
-          // expand
+            case 'class':
+            case 'style':
+            case 'directives':
+              target[propName] = [].concat(sourceValue, targetValue);
+              break;
+            // expand
 
-          case 'on':
-          case 'nativeOn':
-            assign(targetValue, sourceValue, function (listenerName) {
-              if (targetValue[listenerName]) {
-                targetValue[listenerName] = [].concat(sourceValue[listenerName], targetValue[listenerName]);
-              } else {
-                targetValue[listenerName] = sourceValue[listenerName];
-              }
-            });
-            break;
-          // override
+            case 'on':
+            case 'nativeOn':
+              assign(targetValue, sourceValue, function (listenerName) {
+                if (targetValue[listenerName]) {
+                  targetValue[listenerName] = [].concat(sourceValue[listenerName], targetValue[listenerName]);
+                } else {
+                  targetValue[listenerName] = sourceValue[listenerName];
+                }
+              });
+              break;
+            // override
 
-          default:
-            target[propName] = source[propName];
-            break;
+            default:
+              target[propName] = source[propName];
+              break;
+          }
+        } else {
+          target[propName] = sourceValue;
         }
-      } else {
-        target[propName] = sourceValue;
-      }
-    });
+      });
+    };
+
+    for (var i in sources) {
+      _loop(i);
+    }
+
     return target;
   }
 
